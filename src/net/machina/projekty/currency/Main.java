2d5855f565a248ed8529abcadaee52f070c04ba7
Machina <machina123@boun.cr>
2015-12-01 18:38:43 +0100

    Initial commit

---
+++ src/net/machina/projekty/currency/Main.java
@@ -0,0 +1,13 @@
+package net.machina.projekty.currency;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            new MainForm().setVisible(true);
+        });
+    }
+    
+}
